454ae197369ffd84f2ed62e26223beb42558e752
siriuzz <1106901@est.intec.edu.do>
2023-02-22 18:19:58 -0400

    funcionando

--- MayorEnListaRecursion/MayorEnListaRecursion.cpp
+++ MayorEnListaRecursion/MayorEnListaRecursion.cpp
@@ -30,8 +30,9 @@ int EncontrarMayor(int arr[], int low, int high) {
 int main()
 {
 	int nums[100];
+	string cantNums, num;
+
 	while (true) {
-		string cantNums, num;
 		cout << "Cuantos numeros desea insertar? ";
 		cin >> cantNums;
 
@@ -56,24 +57,13 @@ int main()
 		break;
 	}
 
-	int i = 0, maxNum;
-	while (nums[i] != NULL) {
-		i++;
+	int j = 0, maxNum;
+	while (j < stoi(cantNums)) {
+		j++;
 	}
 
-	int high = i;
+	int high = j - 1;
 
-	maxNum = EncontrarMayor(nums, 0, i - 1);
-	cout << "El numero mayor es: " << maxNum;
+	maxNum = EncontrarMayor(nums, 0, high);
+	cout << "El numero menor es: " << maxNum;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
