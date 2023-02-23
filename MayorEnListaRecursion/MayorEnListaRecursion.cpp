7c82d79dfba6879d758a9f5e998e444da8aac43a
siriuzz <1106901@est.intec.edu.do>
2023-02-23 14:04:58 -0400

    comentado

--- MayorEnListaRecursion/MayorEnListaRecursion.cpp
+++ MayorEnListaRecursion/MayorEnListaRecursion.cpp
@@ -17,35 +17,35 @@ bool ValidarNumero(string str)// metodo para comprobar si el dato introducido es
 }
 
 
-int EncontrarMayor(int arr[], int low, int high) {
+int EncontrarMenor(int arr[], int high) {
 	
-	if (high == 0) return arr[0];
+	if (high == 0) return arr[0]; //condicion de finalizacion de la recursion
 
-	int maxElem = EncontrarMayor(arr, low, high - 1);
-	if (arr[high] < maxElem) return arr[high];
-	else return maxElem;
+	int minElem = EncontrarMenor(arr, high - 1); //se encuentra el menor hasta llegar low = 0 y high = 1;
+	if (arr[high] < minElem) return arr[high]; //compara el valor mayor del nivel de recursion con el encontrado mas abajo
+	else return minElem;
 }
 
 
 int main()
 {
-	int nums[100];
+	int nums[100], maxNum;
 	string cantNums, num;
 
 	while (true) {
 		cout << "Cuantos numeros desea insertar? ";
 		cin >> cantNums;
 
-		if (!ValidarNumero(cantNums)) {
+		if (!ValidarNumero(cantNums)) { //cuando cantNums no sea un numero
 			cout << "Cantidad invalida, solo insertar numeros" << endl;
 			continue;
 		}
 
-		for (int i = 0; i < stoi(cantNums); i++) {
+		for (int i = 0; i < stoi(cantNums); i++) { //pedir tantos valores como haya ingresado el usuario
 			cout << "Inserte el valor " << i + 1 << ": ";
 			cin >> num;
 
-			if (!ValidarNumero(num)) {
+			if (!ValidarNumero(num)) { //verificar que sea un numero antes de agregarlo al array
 				cout << "Numero invalido, intente de nuevo" << endl;
 				i--;
 				continue;
@@ -53,17 +53,9 @@ int main()
 
 			nums[i] = stoi(num);
 		}
-
 		break;
 	}
 
-	int j = 0, maxNum;
-	while (j < stoi(cantNums)) {
-		j++;
-	}
-
-	int high = j - 1;
-
-	maxNum = EncontrarMayor(nums, 0, high);
+	maxNum = EncontrarMenor(nums, stoi(cantNums)-1); //funcion recursiva
 	cout << "El numero menor es: " << maxNum;
 }
